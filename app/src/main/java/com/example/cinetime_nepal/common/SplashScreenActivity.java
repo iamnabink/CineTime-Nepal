228703c5cb32998e369dc756d592830a99b940f8
iamnabink <nabrajkhadka43@gmail.com>
2019-11-28 18:42:48 +0545

    added adapter package
    removed minor errors

--- app/src/main/java/com/example/cinetime_nepal/common/SplashScreenActivity.java
+++ app/src/main/java/com/example/cinetime_nepal/common/SplashScreenActivity.java
@@ -1,10 +1,12 @@
-package com.example.cinetime_nepal;
+package com.example.cinetime_nepal.common;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.cinetime_nepal.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
