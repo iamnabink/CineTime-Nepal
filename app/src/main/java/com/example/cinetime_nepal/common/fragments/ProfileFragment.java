22296fc723c7021a67bbeb8fda68e9c80cae94be
iamnabink <nabrajkhadka43@gmail.com>
2019-12-06 22:45:01 +0545

    activity_login added

--- app/src/main/java/com/example/cinetime_nepal/common/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/cinetime_nepal/common/fragments/ProfileFragment.java
@@ -7,31 +7,34 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
-
 import com.example.cinetime_nepal.R;
+import com.example.cinetime_nepal.common.utils.Validator;
 import com.example.cinetime_nepal.member.activities.SigninActivity;
 import com.example.cinetime_nepal.member.fragments.UProfileFragment;
 
 public class ProfileFragment extends Fragment {
-    TextView signupTv;
+    TextView signupTv,emailEt,pwdEt;
     Button signinBtn;
     View view;
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-         view= inflater.inflate(R.layout.fragment_profile, container, false);
-
+        view = inflater.inflate(R.layout.fragment_profile, container, false);
         initVar();
         clickListener();
         return view;
     }
 
     private void initVar() {
-        signupTv=view.findViewById(R.id.signup_tv);
-        signinBtn=view.findViewById(R.id.signin_btn);
+        emailEt=view.findViewById(R.id.email_et);
+        pwdEt=view.findViewById(R.id.pwd_et);
+        signupTv = view.findViewById(R.id.signup_tv);
+        signinBtn = view.findViewById(R.id.signin_btn);
     }
 
     private void clickListener() {
@@ -45,11 +48,24 @@ public class ProfileFragment extends Fragment {
         signinBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                FragmentManager fragmentManager=getActivity().getSupportFragmentManager();
-                FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
-                fragmentTransaction.replace(R.id.profile_f, new UProfileFragment()); //My second Fragment
-                fragmentTransaction.addToBackStack(null);
-                fragmentTransaction.commit();
+                String email = emailEt.getText().toString();
+                String password = pwdEt.getText().toString();
+                if (!Validator.isEmailValid(email)){
+                    emailEt.setError("Invalid email address");
+                }
+                else if (password.isEmpty()){
+                    pwdEt.setError("please enter password");
+                }
+                else if(password.length()<6){
+                    pwdEt.setError("password must be greater than 6 character");
+                }
+                else {
+                    FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                    fragmentTransaction.replace(R.id.profile_f, new UProfileFragment()); //My second Fragment
+                    fragmentTransaction.addToBackStack(null);
+                    fragmentTransaction.commit();
+                }
             }
         });
     }
--- app/src/main/java/com/example/cinetime_nepal/common/utils/Validator.java
+++ app/src/main/java/com/example/cinetime_nepal/common/utils/Validator.java
@@ -1,4 +1,8 @@
 package com.example.cinetime_nepal.common.utils;
 
 public class Validator {
+    public static Boolean isEmailValid(String email){
+        String emailRegex = "^([a-zA-Z0-9_\\-\\.]+)@([a-zA-Z0-9_\\-\\.]+)\\.([a-zA-Z]{2,5})$";
+        return email.matches(emailRegex);
+    }
 }
