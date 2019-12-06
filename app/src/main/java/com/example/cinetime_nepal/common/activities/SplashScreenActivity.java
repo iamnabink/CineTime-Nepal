fa852f7416030002631818b524d9571e0d9e9c12
iamnabink <nabrajkhadka43@gmail.com>
2019-12-07 00:23:06 +0545

    changed splashscreen logo

--- app/src/main/java/com/example/cinetime_nepal/common/activities/SplashScreenActivity.java
+++ app/src/main/java/com/example/cinetime_nepal/common/activities/SplashScreenActivity.java
@@ -21,13 +21,14 @@ public class SplashScreenActivity extends AppCompatActivity {
         setContentView(R.layout.splashscreen);
         logoIv = findViewById(R.id.splash_iv);
         RotateAnimation rotate = new RotateAnimation(0, 360, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
-        rotate.setDuration(900);
+        rotate.setDuration(1200);
         rotate.setInterpolator(new DecelerateInterpolator());
         logoIv.startAnimation(rotate);
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
                 startActivity(new Intent(getApplicationContext(),HomeActivity.class));
+                finish();
             }
         },800);
     }
