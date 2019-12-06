e069c844ddec61fb3f64ccacdef35e090364eb8d
iamnabink <nabrajkhadka43@gmail.com>
2019-12-07 01:03:45 +0545

    added custom dialog

--- app/src/main/java/com/example/cinetime_nepal/common/utils/CustomDialog.java
+++ app/src/main/java/com/example/cinetime_nepal/common/utils/CustomDialog.java
@@ -1,4 +1,31 @@
 package com.example.cinetime_nepal.common.utils;
 
-public class CustomDialog {
+import android.app.Dialog;
+import android.content.Context;
+import android.graphics.drawable.AnimatedVectorDrawable;
+import android.os.Build;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.example.cinetime_nepal.R;
+
+import androidx.annotation.NonNull;
+
+public class CustomDialog extends Dialog {
+    public CustomDialog(@NonNull Context context) {
+        super(context);
+        setCancelable(false);
+        View v = LayoutInflater.from(context).inflate(R.layout.layout_progress,null);
+        ImageView imageView =  v.findViewById(R.id.progress_logo);
+        AnimatedVectorDrawable animatedVectorDrawable = null;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            animatedVectorDrawable = (AnimatedVectorDrawable) context.getDrawable(R.drawable.ic_logo);
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            imageView.setImageDrawable(animatedVectorDrawable);
+        }
+        animatedVectorDrawable.start();
+        setContentView(v);
+    }
 }
