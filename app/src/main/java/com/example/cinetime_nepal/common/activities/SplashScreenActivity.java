7039dd7ef43bd27cde031fbd764cea0ddd1ed912
iamnabink <nabrajkhadka43@gmail.com>
2019-12-06 23:59:33 +0545

    added handler class for splashscreen

--- app/src/main/java/com/example/cinetime_nepal/common/activities/SplashScreenActivity.java
+++ app/src/main/java/com/example/cinetime_nepal/common/activities/SplashScreenActivity.java
@@ -1,14 +1,34 @@
 package com.example.cinetime_nepal.common.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.DecelerateInterpolator;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+
 import com.example.cinetime_nepal.R;
 
 public class SplashScreenActivity extends AppCompatActivity {
+    ImageView logoIv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splashscreen);
+        logoIv = findViewById(R.id.splash_iv);
+        RotateAnimation rotate = new RotateAnimation(0, 360, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        rotate.setDuration(900);
+        rotate.setInterpolator(new DecelerateInterpolator());
+        logoIv.startAnimation(rotate);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(),HomeActivity.class));
+            }
+        },800);
     }
 }
