197e93115b84ab4a5280558835a5a178e9107720
iamnabink <nabrajkhadka43@gmail.com>
2019-12-07 15:46:54 +0545

    added profile fragment for not logged in user

--- app/src/main/java/com/example/cinetime_nepal/common/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/cinetime_nepal/common/fragments/ProfileFragment.java
@@ -1,21 +1,12 @@
 package com.example.cinetime_nepal.common.fragments;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.TextView;
-
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 import com.example.cinetime_nepal.R;
-import com.example.cinetime_nepal.common.utils.Validator;
-import com.example.cinetime_nepal.member.activities.SigninActivity;
-import com.example.cinetime_nepal.member.fragments.UProfileFragment;
 
 public class ProfileFragment extends Fragment {
     View view;
