7922b5885e8d45af43d23beade4c7ed1dd315ce2
iamnabink <nabrajkhadka43@gmail.com>
2019-12-09 14:06:53 +0545

    add movie model

--- app/src/main/java/com/example/cinetime_nepal/common/models/Movie.java
+++ app/src/main/java/com/example/cinetime_nepal/common/models/Movie.java
@@ -1,13 +1,115 @@
 package com.example.cinetime_nepal.common.models;
 
-public class Movie {
-    private String name;
-    private int img_url;
-    private String genre;
+public abstract class Movie {
 
-    public Movie(String name, int img_url, String genre) {
-        this.name = name;
-        this.img_url = img_url;
+    private String updated_at;
+    private String created_at;
+    private int status;
+    private String release_date;
+    private String youtube_trailer_url;
+    private String poster_url;
+    private String directors;
+    private String casts;
+    private String synopsis;
+    private String language;
+    private String run_time;
+    private String genre;
+    private String name;
+    private int id;
+
+    public String getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(String updated_at) {
+        this.updated_at = updated_at;
+    }
+
+    public String getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(String created_at) {
+        this.created_at = created_at;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getRelease_date() {
+        return release_date;
+    }
+
+    public void setRelease_date(String release_date) {
+        this.release_date = release_date;
+    }
+
+    public String getYoutube_trailer_url() {
+        return youtube_trailer_url;
+    }
+
+    public void setYoutube_trailer_url(String youtube_trailer_url) {
+        this.youtube_trailer_url = youtube_trailer_url;
+    }
+
+    public String getPoster_url() {
+        return poster_url;
+    }
+
+    public void setPoster_url(String poster_url) {
+        this.poster_url = poster_url;
+    }
+
+    public String getDirectors() {
+        return directors;
+    }
+
+    public void setDirectors(String directors) {
+        this.directors = directors;
+    }
+
+    public String getCasts() {
+        return casts;
+    }
+
+    public void setCasts(String casts) {
+        this.casts = casts;
+    }
+
+    public String getSynopsis() {
+        return synopsis;
+    }
+
+    public void setSynopsis(String synopsis) {
+        this.synopsis = synopsis;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public String getRun_time() {
+        return run_time;
+    }
+
+    public void setRun_time(String run_time) {
+        this.run_time = run_time;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
         this.genre = genre;
     }
 
@@ -19,19 +121,11 @@ public class Movie {
         this.name = name;
     }
 
-    public int getImg_url() {
-        return img_url;
+    public int getId() {
+        return id;
     }
 
-    public void setImg_url(int img_url) {
-        this.img_url = img_url;
-    }
-
-    public String getGenre() {
-        return genre;
-    }
-
-    public void setGenre(String genre) {
-        this.genre = genre;
+    public void setId(int id) {
+        this.id = id;
     }
 }
