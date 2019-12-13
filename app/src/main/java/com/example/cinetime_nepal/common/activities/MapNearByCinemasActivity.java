8fcedde47fdc09472754fbf3683177c87e3ae616
iamnabink <nabrajkhadka43@gmail.com>
2019-12-13 13:02:19 +0545

    added map API

--- app/src/main/java/com/example/cinetime_nepal/common/activities/MapNearByCinemasActivity.java
+++ app/src/main/java/com/example/cinetime_nepal/common/activities/MapNearByCinemasActivity.java
@@ -1,4 +1,47 @@
 package com.example.cinetime_nepal.common.activities;
 
-public class MapNearByCinemasActivity {
+import android.os.Bundle;
+
+import com.example.cinetime_nepal.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.FragmentActivity;
+
+public class MapNearByCinemasActivity extends FragmentActivity implements OnMapReadyCallback {
+    SupportMapFragment mapFragment;
+    GoogleMap gMap;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map_nearby_cinemas);
+        mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map_fragment);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        gMap= googleMap;
+        int width = getResources().getDisplayMetrics().widthPixels;
+        int height = getResources().getDisplayMetrics().heightPixels;
+        int padding = (int) (width * 0.12); // offset from edges of the map 12% of screen
+        LatLng bigMovies = new LatLng(27.711333, 85.326510);
+        gMap.addMarker(new MarkerOptions().position(bigMovies).title("Big Movies"));
+        LatLngBounds.Builder builder = new LatLngBounds.Builder();
+        PolylineOptions polylineOptions = new PolylineOptions();
+        polylineOptions.add(bigMovies);
+        builder.include(bigMovies);
+        Polyline polyline = gMap.addPolyline(polylineOptions);
+        LatLngBounds bounds = builder.build();
+        gMap.animateCamera(CameraUpdateFactory.newLatLngBounds(bounds, width, height, padding));
+
+    }
 }
--- app/src/main/java/com/example/cinetime_nepal/common/fragments/HallFragment.java
+++ app/src/main/java/com/example/cinetime_nepal/common/fragments/HallFragment.java
@@ -1,18 +1,32 @@
 package com.example.cinetime_nepal.common.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
+
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.example.cinetime_nepal.R;
+import com.example.cinetime_nepal.common.activities.MapNearByCinemasActivity;
 
 public class HallFragment extends Fragment {
+    View view;
+    ImageView mapNearme;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_hall, container, false);
+        view= inflater.inflate(R.layout.fragment_hall, container, false);
+        mapNearme=view.findViewById(R.id.map_nearme_ic);
+        mapNearme.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getContext(), MapNearByCinemasActivity.class));
+            }
+        });
+         return view;
     }
 }
