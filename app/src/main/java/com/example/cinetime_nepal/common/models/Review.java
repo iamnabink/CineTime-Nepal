2d4a34ac6e67ec139500d387eb169e5378803408
iamnabink <nabrajkhadka43@gmail.com>
2019-12-17 15:34:08 +0545

    added review class

---
+++ app/src/main/java/com/example/cinetime_nepal/common/models/Review.java
@@ -0,0 +1,69 @@
+package com.example.cinetime_nepal.common.models;
+
+public class Review {
+
+    private User user;
+    private String created_at;
+    private int rating_count;
+    private String comment_msg;
+    private int movie_id;
+    private int user_id;
+    private int id;
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(String created_at) {
+        this.created_at = created_at;
+    }
+
+    public int getRating_count() {
+        return rating_count;
+    }
+
+    public void setRating_count(int rating_count) {
+        this.rating_count = rating_count;
+    }
+
+    public String getComment_msg() {
+        return comment_msg;
+    }
+
+    public void setComment_msg(String comment_msg) {
+        this.comment_msg = comment_msg;
+    }
+
+    public int getMovie_id() {
+        return movie_id;
+    }
+
+    public void setMovie_id(int movie_id) {
+        this.movie_id = movie_id;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+}
---
+++ app/src/main/java/com/example/cinetime_nepal/common/models/User.java
@@ -0,0 +1,67 @@
+package com.example.cinetime_nepal.common.models;
+
+public class User {
+    private String updated_at;
+    private String created_at;
+    private String bio;
+    private String profile_pic_url;
+    private String email;
+    private String name;
+    private int id;
+
+    public String getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(String updated_at) {
+        this.updated_at = updated_at;
+    }
+
+    public String getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(String created_at) {
+        this.created_at = created_at;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public String getProfile_pic_url() {
+        return profile_pic_url;
+    }
+
+    public void setProfile_pic_url(String profile_pic_url) {
+        this.profile_pic_url = profile_pic_url;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
