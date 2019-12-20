e2764a3ef561f72df0df64ec7cee78b2d7a4218b
iamnabink <nabrajkhadka43@gmail.com>
2019-12-20 21:20:42 +0545

    API Change

--- app/src/main/java/com/example/cinetime_nepal/common/network/API.java
+++ app/src/main/java/com/example/cinetime_nepal/common/network/API.java
@@ -6,9 +6,8 @@ public class API {
     public static String loginUrl=baseUrl+"auth/login";
     public static String updateUrl=baseUrl+"auth/update";
     public static String changePwdUrl=baseUrl+"auth/changepwd";
-    private static String baseUrlMovie  = "https://cinetimenepal.whoamie.com/api/";
-    public static String upcomingMovieUrl  =baseUrlMovie+ "v1/movies/upcoming";
-    public static String nowShowingMovieUrl  =baseUrlMovie+ "v1/movies/nowshowing";
-    public static String showtimeMovieUrl  =baseUrlMovie+"v1/showtimes/showtime";
-    public static String getMovieReviews  =baseUrlMovie+"reviews";
+    public static String upcomingMovieUrl  =baseUrl+ "v1/movies/upcoming";
+    public static String nowShowingMovieUrl  =baseUrl+ "v1/movies/nowshowing";
+    public static String showtimeMovieUrl  =baseUrl+"v1/showtimes/showtime";
+    public static String getMovieReviews  =baseUrl+"reviews";
 }
