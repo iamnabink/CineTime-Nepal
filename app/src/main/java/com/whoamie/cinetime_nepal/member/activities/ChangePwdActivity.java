7b30abd503da39f0873694da195899371a707238
iamnabink <nabrajkhadka43@gmail.com>
2020-01-21 16:25:21 +0545

    issue solved with change password

--- app/src/main/java/com/whoamie/cinetime_nepal/member/activities/ChangePwdActivity.java
+++ app/src/main/java/com/whoamie/cinetime_nepal/member/activities/ChangePwdActivity.java
@@ -3,7 +3,6 @@ package com.whoamie.cinetime_nepal.member.activities;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
@@ -26,11 +25,12 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ChangePwdActivity extends AppCompatActivity {
-    EditText oldPwdEt,newPwd,cfmPwd;
+    EditText oldPwdEt, newPwd, cfmPwd;
     Button changeBtn;
     ProgressDialog dialog;
     SharedPreferences.Editor editor;
     SharedPreferences preferences;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,45 +43,37 @@ public class ChangePwdActivity extends AppCompatActivity {
         changeBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                changeBtn.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        String oldPassword=oldPwdEt.getText().toString();
-                        String newPassword = newPwd.getText().toString();
-                        String confirmPassword = cfmPwd.getText().toString();
-                        if (oldPassword.isEmpty() && newPassword.isEmpty() && confirmPassword.isEmpty()){
-                           oldPwdEt.setError("Please enter old password");
-                           newPwd.setError("Please enter new password");
-                           cfmPwd.setError("Please confirm new password");
-                        }
-                        else if(!newPassword.equals(confirmPassword)){
-                            cfmPwd.setError("confirm password doesn't match");
-                        }
-                        else {
-                            changePassword();
-                        }
-                    }
-                });
+                String oldPassword = oldPwdEt.getText().toString();
+                String newPassword = newPwd.getText().toString();
+                String confirmPassword = cfmPwd.getText().toString();
+                if (oldPassword.isEmpty() && newPassword.isEmpty() && confirmPassword.isEmpty()) {
+                    oldPwdEt.setError("Please enter old password");
+                    newPwd.setError("Please enter new password");
+                    cfmPwd.setError("Please confirm new password");
+                } else if (!newPassword.equals(confirmPassword)) {
+                    cfmPwd.setError("confirm password doesn't match");
+                } else {
+                    changePassword();
+                }
             }
         });
     }
 
     private void initVar() {
-        oldPwdEt=findViewById(R.id.old_pwd_et);
+        oldPwdEt = findViewById(R.id.old_pwd_et);
         newPwd = findViewById(R.id.new_pwd);
-        cfmPwd=findViewById(R.id.cfm_pwd);
-        changeBtn=findViewById(R.id.change_btn);
-        dialog = new ProgressDialog(getApplicationContext());
+        cfmPwd = findViewById(R.id.cfm_pwd);
+        changeBtn = findViewById(R.id.change_btn);
+        dialog = new ProgressDialog(this);
         Window window = dialog.getWindow();
 //        window.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, 800);
         dialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
-        preferences = getSharedPreferences(SharedPref.key_shared_pref,MODE_PRIVATE);
+        preferences = getSharedPreferences(SharedPref.key_shared_pref, MODE_PRIVATE);
         editor = preferences.edit();
     }
+
     private void changePassword() {
         dialog.show();
-        Window window = dialog.getWindow();
-        window.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, 800);
         JSONObject jsonObject = new JSONObject(); // creating json object
         try {
             jsonObject.put("old_password", oldPwdEt.getText().toString());
@@ -95,10 +87,10 @@ public class ChangePwdActivity extends AppCompatActivity {
             public void onResponse(JSONObject response) {
                 dialog.dismiss();
                 try {
-                    if (response.getBoolean("status")){
+                    if (response.getBoolean("status")) {
                         JSONObject dataObject = response.getJSONObject(SharedPref.key_data_details);
                         JSONObject tokenObject = dataObject.getJSONObject(SharedPref.key_user_token); //retrieving new token
-                        String tokenString=tokenObject.toString(); //converting json token object into json string format
+                        String tokenString = tokenObject.toString(); //converting json token object into json string format
                         editor.putString(SharedPref.key_user_token, tokenString); //overwriting new token string in to sharedprefs
                         editor.commit(); //commits, overwrites the data
                         Toast.makeText(ChangePwdActivity.this, response.getString("message"), Toast.LENGTH_SHORT).show();
@@ -110,6 +102,7 @@ public class ChangePwdActivity extends AppCompatActivity {
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
+                System.out.println(error);
                 dialog.dismiss();
                 Toast.makeText(ChangePwdActivity.this, "Server error", Toast.LENGTH_SHORT).show();
             }
