89ee499aac92443f07ace1c9bda34bddac259b86
iamnabink <nabrajkhadka43@gmail.com>
2020-02-15 00:57:18 +0545

    added halls list to cinema fragment

--- app/src/main/java/com/whoamie/cinetime_nepal/common/fragments/HallFragment.java
+++ app/src/main/java/com/whoamie/cinetime_nepal/common/fragments/HallFragment.java
@@ -1,7 +1,9 @@
 package com.whoamie.cinetime_nepal.common.fragments;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -11,17 +13,24 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.google.gson.Gson;
 import com.whoamie.cinetime_nepal.R;
 import com.whoamie.cinetime_nepal.common.adapter.HallAdapter;
 import com.whoamie.cinetime_nepal.common.interfaces.AdapterClickListener;
 import com.whoamie.cinetime_nepal.common.models.Hall;
 import com.whoamie.cinetime_nepal.common.network.API;
 import com.whoamie.cinetime_nepal.common.network.AuthenticatedJSONRequest;
+import com.whoamie.cinetime_nepal.common.network.RestClient;
+import com.whoamie.cinetime_nepal.common.utils.CheckConnectivity;
+import com.whoamie.cinetime_nepal.common.utils.SharedPref;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.lang.reflect.Method;
@@ -32,6 +41,7 @@ public class HallFragment extends Fragment {
     RecyclerView recyclerView;
     HallAdapter adapter;
     ArrayList<Hall> halls = new ArrayList<>();
+    Context context;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -43,6 +53,12 @@ public class HallFragment extends Fragment {
         initRecyclerView();
         loadData();
         return view;
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        this.context = context;
+        super.onAttach(context);
     }
 
     private void initRecyclerView() {
@@ -62,19 +78,35 @@ public class HallFragment extends Fragment {
     }
 
     private void loadData() {
+        halls.clear();
         AuthenticatedJSONRequest request = new AuthenticatedJSONRequest(getContext(), Request.Method.GET, API.getHallDetails, null, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
-                
+                try {
+                    JSONObject jsonObject = response.getJSONObject(SharedPref.key_data_details);
+                    JSONArray jsonArray = jsonObject.getJSONArray("halls");
+                    for (int i = 0;i<jsonArray.length();i++){
+                        JSONObject object = jsonArray.getJSONObject(i);
+                        Hall hall = new Gson().fromJson(object.toString(),Hall.class);
+                        halls.add(hall);
+                    }
+                    adapter.notifyDataSetChanged();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
             }
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-
+                Toast.makeText(getContext(), "Server error", Toast.LENGTH_SHORT).show();
             }
         });
+        if (CheckConnectivity.isNetworkAvailable(context)){
+            RestClient.getInstance(getContext()).addToRequestQueue(request);
+        }
+        else {
+            Toast.makeText(context, "No network available", Toast.LENGTH_SHORT).show();
+        }
 
     }
-
-
 }
