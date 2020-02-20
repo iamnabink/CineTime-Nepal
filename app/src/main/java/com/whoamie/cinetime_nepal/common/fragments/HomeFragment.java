97d48559df13df93ced40bec65c61977b9e0adf1
iamnabink <nabrajkhadka43@gmail.com>
2020-02-20 21:45:37 +0545

    added youtube API

--- app/src/main/java/com/whoamie/cinetime_nepal/common/fragments/HomeFragment.java
+++ app/src/main/java/com/whoamie/cinetime_nepal/common/fragments/HomeFragment.java
@@ -8,15 +8,41 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerFragment;
+import com.google.android.youtube.player.YouTubePlayerView;
 import com.whoamie.cinetime_nepal.R;
+import com.whoamie.cinetime_nepal.common.YoutubePlayer.YoutubeConfig;
 
 public class HomeFragment extends Fragment {
 
+    YouTubePlayerFragment playerFragment;
+    View view;
+    YouTubePlayer.OnInitializedListener listener;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
+        view = inflater.inflate(R.layout.fragment_home, container, false);
         ((AppCompatActivity) getActivity()).getSupportActionBar().show();
-        return root;
+        initiateVideoplayer();
+        return view;
     }
+
+    private void initiateVideoplayer() {
+//        playerFragment = (YouTubePlayerFragment) getFragmentManager().findFragmentById(R.id.youtube_frag);
+        listener = new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                youTubePlayer.loadVideo("5KXLCkhhWbM");
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+            }
+        };
+        playerFragment.initialize(YoutubeConfig.getApiKey(),listener);
+    }
+
 }
