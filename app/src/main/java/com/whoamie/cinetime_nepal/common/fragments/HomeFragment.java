ce5e5fbf855c987f52b8297044f61acc69965e6f
iamnabink <nabrajkhadka43@gmail.com>
2020-02-24 22:24:38 +0545

    removed youtube player library

--- app/src/main/java/com/whoamie/cinetime_nepal/common/fragments/HomeFragment.java
+++ app/src/main/java/com/whoamie/cinetime_nepal/common/fragments/HomeFragment.java
@@ -7,34 +7,18 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-
-import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer;
-import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener;
-import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView;
 import com.whoamie.cinetime_nepal.R;
 
 public class HomeFragment extends Fragment {
-    YouTubePlayerView youTubePlayerView;
     View view;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_home, container, false);
         ((AppCompatActivity) getActivity()).getSupportActionBar().show();
-        initiateVideoplayer();
+//        initiateVideoplayer();
         return view;
     }
 
-    private void initiateVideoplayer() {
-        youTubePlayerView=view.findViewById(R.id.youtube_player_view);
-        getLifecycle().addObserver(youTubePlayerView);
-        youTubePlayerView.addYouTubePlayerListener(new AbstractYouTubePlayerListener() {
-            @Override
-            public void onReady(YouTubePlayer youTubePlayer) {
-                String videoId = "S0Q4gqBUs7c";
-                youTubePlayer.loadVideo(videoId, 0);
-                youTubePlayer.pause();
-            }
-        });
-    }
+//    private void initiateVideoplayer() {
 }
