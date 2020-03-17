6ee56e887b51ebc5c57e1f2dd8edfc9204a55412
iamnabink <nabrajkhadka43@gmail.com>
2020-03-17 19:16:57 +0545

    updated ShowTimeClass

--- app/src/main/java/com/whoamie/cinetime_nepal/common/models/ShowTime.java
+++ app/src/main/java/com/whoamie/cinetime_nepal/common/models/ShowTime.java
@@ -15,47 +15,24 @@ public class ShowTime {
         return hall;
     }
 
-    public void setHall(Hall hall) {
-        this.hall = hall;
-    }
-
     public List<Time> getTimes() {
         return times;
     }
 
-    public void setTimes(List<Time> times) {
-        this.times = times;
-    }
     public String getShow_date() {
         return show_date;
-    }
-
-    public void setShow_date(String show_date) {
-        this.show_date = show_date;
     }
 
     public int getHall_id() {
         return hall_id;
     }
 
-    public void setHall_id(int hall_id) {
-        this.hall_id = hall_id;
-    }
-
     public int getMovie_id() {
         return movie_id;
     }
 
-    public void setMovie_id(int movie_id) {
-        this.movie_id = movie_id;
-    }
-
     public int getId() {
         return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
     }
 
 }
