6fab387f742634a5ee8b7070c38b9e5900ea183d
iamnabink <nabrajkhadka43@gmail.com>
2020-03-19 22:10:47 +0545

    commented some unnecessary code

--- app/src/main/java/com/whoamie/cinetime_nepal/common/activities/SettingActivity.java
+++ app/src/main/java/com/whoamie/cinetime_nepal/common/activities/SettingActivity.java
@@ -22,23 +22,23 @@ public class SettingActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
-        expandableView = findViewById(R.id.expandableView);
-        arrowBtn = findViewById(R.id.arrowBtn);
-        cardView = findViewById(R.id.cardView);
-
-        arrowBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (expandableView.getVisibility()==View.GONE){
-                    TransitionManager.beginDelayedTransition(cardView, new AutoTransition());
-                    expandableView.setVisibility(View.VISIBLE);
-                    arrowBtn.setBackgroundResource(R.drawable.ic_arrow_drop_up_black_24dp);
-                } else {
-                    TransitionManager.beginDelayedTransition(cardView, new AutoTransition());
-                    expandableView.setVisibility(View.GONE);
-                    arrowBtn.setBackgroundResource(R.drawable.ic_arrow_drop_down_black_24dp);
-                }
-            }
-        });
+//        expandableView = findViewById(R.id.expandableView);
+//        arrowBtn = findViewById(R.id.arrowBtn);
+//        cardView = findViewById(R.id.cardView);
+//
+//        arrowBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if (expandableView.getVisibility()==View.GONE){
+//                    TransitionManager.beginDelayedTransition(cardView, new AutoTransition());
+//                    expandableView.setVisibility(View.VISIBLE);
+//                    arrowBtn.setBackgroundResource(R.drawable.ic_arrow_drop_up_black_24dp);
+//                } else {
+//                    TransitionManager.beginDelayedTransition(cardView, new AutoTransition());
+//                    expandableView.setVisibility(View.GONE);
+//                    arrowBtn.setBackgroundResource(R.drawable.ic_arrow_drop_down_black_24dp);
+//                }
+//            }
+//        });
     }
 }
