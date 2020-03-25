2f2566b1429d3786ddf5d06506e2ee18e7cc51b2
iamnabink <nabrajkhadka43@gmail.com>
2020-03-25 17:39:20 +0545

    customized profile page

--- app/src/main/java/com/whoamie/cinetime_nepal/member/fragments/ReviewFragment.java
+++ app/src/main/java/com/whoamie/cinetime_nepal/member/fragments/ReviewFragment.java
@@ -1,8 +1,11 @@
 package com.whoamie.cinetime_nepal.member.fragments;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,16 +15,28 @@ import android.widget.Toast;
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.facebook.shimmer.ShimmerFrameLayout;
 import com.whoamie.cinetime_nepal.R;
 import com.whoamie.cinetime_nepal.common.network.API;
 import com.whoamie.cinetime_nepal.common.network.AuthenticatedJSONRequest;
 import com.whoamie.cinetime_nepal.common.network.RestClient;
 import com.whoamie.cinetime_nepal.common.utils.CheckConnectivity;
+import com.whoamie.cinetime_nepal.member.adapters.FavMovieAdapter;
+import com.whoamie.cinetime_nepal.member.adapters.MyReviewAdapter;
+import com.whoamie.cinetime_nepal.member.models.FavMovie;
+import com.whoamie.cinetime_nepal.member.models.MyReview;
 
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+
 public class ReviewFragment extends Fragment {
     View view;
+    Context context;
+    ShimmerFrameLayout shimmerFrameLayout;
+    RecyclerView recyclerView;
+    MyReviewAdapter adapter;
+    ArrayList<MyReview> reviews = new ArrayList<>();
 //    public ProfileReviewFragment(int userId) {
 //        this.userId = userId;
 //    }
@@ -45,11 +60,26 @@ public class ReviewFragment extends Fragment {
     private void initViews() {
 
     }
+//    @Override
+//    public void onPause() {
+//        super.onPause();
+//        shimmerFrameLayout.startShimmer();
+//    }
+//
+//    @Override
+//    public void onResume() {
+//        super.onResume();
+//        shimmerFrameLayout.stopShimmer();
+//    }
 
     private void clickListner() {
 
     }
-
+    @Override
+    public void onAttach(@NonNull Context context) {
+        this.context=context;
+        super.onAttach(context);
+    }
     private void loadReviews() {
         AuthenticatedJSONRequest authenticatedJSONRequest = new AuthenticatedJSONRequest(getContext(), Request.Method.POST, API.getUserReview, null, new Response.Listener<JSONObject>() {
             @Override
@@ -62,7 +92,7 @@ public class ReviewFragment extends Fragment {
 
             }
         });
-        if (CheckConnectivity.isNetworkAvailable(getContext())){
+        if (CheckConnectivity.isNetworkAvailable(context)){
             RestClient.getInstance(getContext()).addToRequestQueue(authenticatedJSONRequest);
         }
         else {
