6fbfe4afc40da36888a6fe8ad1ba3791de9c9755
iamnabink <nabrajkhadka43@gmail.com>
2020-04-20 04:28:25 +0545

    added custom image marker in marker

--- app/src/main/java/com/whoamie/cinetime_nepal/common/activities/MapNearByCinemasActivity.java
+++ app/src/main/java/com/whoamie/cinetime_nepal/common/activities/MapNearByCinemasActivity.java
@@ -4,9 +4,12 @@ import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
 import android.location.Location;
 import android.location.LocationManager;
@@ -14,11 +17,15 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.Looper;
 import android.provider.Settings;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.CustomTarget;
+import com.bumptech.glide.request.transition.Transition;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
@@ -38,6 +45,8 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.whoamie.cinetime_nepal.common.models.Hall;
+import com.whoamie.cinetime_nepal.common.utils.SharedPref;
+import com.whoamie.cinetime_nepal.member.models.User;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -61,27 +70,40 @@ public class MapNearByCinemasActivity extends FragmentActivity implements OnMapR
     private GoogleMap mMap;
     Double lat, lon;
     ImageView satalitveIv, defaultIv;
+    private ImageView mMarkerImageView;
     FrameLayout mCustomMarkerView;
+//    User user;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map_nearby_cinemas);
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        mCustomMarkerView = (FrameLayout) ((LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE)).inflate(R.layout.view_custom_marker, null);
+        mMarkerImageView = mCustomMarkerView.findViewById(R.id.profile_image);
+//        userSharedPrefData();
         handleData();
         checkPermssion();
     }
 
+//    private void userSharedPrefData() {
+//        SharedPreferences preferences = getSharedPreferences(SharedPref.key_shared_pref,MODE_PRIVATE);
+//        String userData = preferences.getString(SharedPref.key_user_details,null);
+//        if (userData!=null){
+//            user = new Gson().fromJson(userData,User.class);
+//        }
+//    }
+
     private void handleData() {
         String data = getIntent().getStringExtra("halldata");
-        if (data!=null){
+        if (data != null) {
             try {
                 JSONObject object = new JSONObject(data);
                 JSONArray array = object.getJSONArray("values");
-                for (int i = 0; i<array.length();i++){
+                for (int i = 0; i < array.length(); i++) {
                     JSONObject valuespair = array.getJSONObject(i);
                     JSONObject hallObject = valuespair.getJSONObject("nameValuePairs");
-                    Hall hall = new Gson().fromJson(hallObject.toString(),Hall.class);
+                    Hall hall = new Gson().fromJson(hallObject.toString(), Hall.class);
                     halls.add(hall);
                 }
             } catch (JSONException e) {
@@ -197,16 +219,22 @@ public class MapNearByCinemasActivity extends FragmentActivity implements OnMapR
             }
         });
         mMap = googleMap;
+        MarkerOptions markerOptions = new MarkerOptions();
 //        mMap.getMapType();
         mMap.setMyLocationEnabled(true);
 //        mMap.getUiSettings().setZoomControlsEnabled(true);
 //        mMap.getUiSettings().setZoomGesturesEnabled(true);
         LatLng latLng = new LatLng(lat, lon); //current location
-//        createMarker(27.709828, 85.326693, "Big Movies", "City Centre");
+//        markerOptions.position(latLng);
+//        markerOptions.title("You are here");
+//        if (user.getProfile_pic_url()!=null){
+//
+//        }
         mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng)); //for current location of user
         mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
-        for(int i = 0 ; i < halls.size() ; i++) {
-            createMarker(halls.get(i).getLat(), halls.get(i).getLon(), halls.get(i).getName(), halls.get(i).getLocation()); //.showInfoWindow()
+        for (int i = 0; i < halls.size(); i++) {
+            addCustomMarker(googleMap, halls.get(i));
+//            createMarker(halls.get(i).getLat(), halls.get(i).getLon(), halls.get(i).getName(), halls.get(i).getLocation()); //.showInfoWindow()
         }
 //        Circle circle = mMap.addCircle(new CircleOptions()
 //                .center(latLng)
@@ -215,22 +243,86 @@ public class MapNearByCinemasActivity extends FragmentActivity implements OnMapR
 //                .fillColor(Color.parseColor("#500084d3")));
 
     }
-    protected Marker createMarker(double latitude, double longitude, String title, String snippet) {
-        return mMap.addMarker(new MarkerOptions()
-                .position(new LatLng(latitude, longitude))
-                .anchor(0.5f, 0.5f)
-                .title(title)
-                .snippet(snippet)
-                .icon(bitmapDescriptorFromVector(this, R.drawable.ic_marker_icon)));
+//    protected Marker createMarker(double latitude, double longitude, String title, String snippet) {
+//        return mMap.addMarker(new MarkerOptions()
+//                .position(new LatLng(latitude, longitude))
+//                .anchor(0.5f, 0.5f)
+//                .title(title)
+//                .snippet(snippet)
+//                .icon(bitmapDescriptorFromVector(this, R.drawable.ic_marker_icon)));
+//    }
+
+    //    private BitmapDescriptor bitmapDescriptorFromVector(Context context, int vectorResId) {
+//        Drawable vectorDrawable = ContextCompat.getDrawable(context, vectorResId);
+//        vectorDrawable.setBounds(0, 0, vectorDrawable.getIntrinsicWidth(), vectorDrawable.getIntrinsicHeight());
+//        Bitmap bitmap = Bitmap.createBitmap(vectorDrawable.getIntrinsicWidth(), vectorDrawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+//        Canvas canvas = new Canvas(bitmap);
+//        vectorDrawable.draw(canvas);
+//        return BitmapDescriptorFactory.fromBitmap(bitmap);
+//    }
+    private void addCustomMarker(final GoogleMap googleMap, final Hall hall) {
+
+        // adding a marker on map with image from  drawable
+       /* mGoogleMap.addMarker(new MarkerOptions()
+                .position(mDummyLatLng)
+                .icon(BitmapDescriptorFactory.fromBitmap(getMarkerBitmapFromView(mCustomMarkerView, R.drawable.avatar))));*/
+
+        // adding a marker with image from URL using glide image loading library
+        Glide.with(this)
+                .asBitmap().
+                load(hall.getProfile_pic_url()).placeholder(R.drawable.no_image_placeholder)
+                .fitCenter()
+                .into(new CustomTarget<Bitmap>() {
+                    @Override
+                    public void onResourceReady(@NonNull Bitmap resource, @Nullable Transition<? super Bitmap> transition) {
+                        googleMap.addMarker(new MarkerOptions()
+                                .position(new LatLng(hall.getLat(), hall.getLon()))
+                                .title(hall.getName())
+                                .snippet(hall.getLocation())
+                                .icon(BitmapDescriptorFactory.fromBitmap(getMarkerBitmapFromView(mCustomMarkerView, resource))));
+                        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(hall.getLat(), hall.getLon()), 13f));
+                    }
+
+                    @Override
+                    public void onLoadCleared(@Nullable Drawable placeholder) {
+
+                    }
+                });
     }
 
-    private BitmapDescriptor bitmapDescriptorFromVector(Context context, int vectorResId) {
-        Drawable vectorDrawable = ContextCompat.getDrawable(context, vectorResId);
-        vectorDrawable.setBounds(0, 0, vectorDrawable.getIntrinsicWidth(), vectorDrawable.getIntrinsicHeight());
-        Bitmap bitmap = Bitmap.createBitmap(vectorDrawable.getIntrinsicWidth(), vectorDrawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
-        Canvas canvas = new Canvas(bitmap);
-        vectorDrawable.draw(canvas);
-        return BitmapDescriptorFactory.fromBitmap(bitmap);
+
+//    private Bitmap getMarkerBitmapFromView(View view, @DrawableRes int resId) {
+//
+//        mMarkerImageView.setImageResource(resId);
+//        view.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED);
+//        view.layout(0, 0, view.getMeasuredWidth(), view.getMeasuredHeight());
+//        view.buildDrawingCache();
+//        Bitmap returnedBitmap = Bitmap.createBitmap(view.getMeasuredWidth(), view.getMeasuredHeight(),
+//                Bitmap.Config.ARGB_8888);
+//        Canvas canvas = new Canvas(returnedBitmap);
+//        canvas.drawColor(Color.WHITE, PorterDuff.Mode.SRC_IN);
+//        Drawable drawable = view.getBackground();
+//        if (drawable != null)
+//            drawable.draw(canvas);
+//        view.draw(canvas);
+//        return returnedBitmap;
+//    }
+
+    private Bitmap getMarkerBitmapFromView(View view, Bitmap bitmap) {
+
+        mMarkerImageView.setImageBitmap(bitmap);
+        view.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED);
+        view.layout(0, 0, view.getMeasuredWidth(), view.getMeasuredHeight());
+        view.buildDrawingCache();
+        Bitmap returnedBitmap = Bitmap.createBitmap(view.getMeasuredWidth(), view.getMeasuredHeight(),
+                Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(returnedBitmap);
+        canvas.drawColor(Color.WHITE, PorterDuff.Mode.SRC_IN);
+        Drawable drawable = view.getBackground();
+        if (drawable != null)
+            drawable.draw(canvas);
+        view.draw(canvas);
+        return returnedBitmap;
     }
 
 }
